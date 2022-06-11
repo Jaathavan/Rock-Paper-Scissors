7b6dee86af8eaa5b1500f2c2e414a419583ff084
Jaathavan <rjaathu@outlook.com>
2022-06-10 22:24:00 -0400

    working and complete rps, ready to merge

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -21,10 +21,28 @@ function playRound(playerSelection, computerSelection) {
         return "Tie! Both of you selected " + computerSelection + "!";
     }
     else if ((playerSelection.match(rockRegex) && computerSelection.match(paperRegex)) || (playerSelection.match(paperRegex) && computerSelection.match(scissorsRegex)) || (playerSelection.match(scissorsRegex) && computerSelection.match(rockRegex)) ) {
-        return "You Lose! You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " while the computer selected " + computerSelection + "!"; 
+        const score = document.querySelector('#computerScore');
+        let points = Number(score.textContent);
+        points++;
+        score.textContent = points;
+        if (points === 5) {
+            return "RIP!!! You lost to the computer!!!"
+        }
+        else {
+            return "You Lose! You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " while the computer selected " + computerSelection + "!"; 
+        }
     }
     else {
-        return "You Won! You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " while the computer selected " + computerSelection + "!";
+        const score = document.querySelector('#playerScore');
+        let points = Number(score.textContent);
+        points++;
+        score.textContent = points;
+        if (points === 5) {
+           return "Congratulations!!! You Won The Game!!!" 
+        }
+        else {
+            return "You Won! You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " while the computer selected " + computerSelection + "!";
+        }
     }
 }
 
@@ -32,13 +50,17 @@ function playRound(playerSelection, computerSelection) {
 function game(choice) {
     const results = document.querySelector('#results');
     results.textContent = playRound(choice, computerPlay());
- }
+}
 
- const btn = document.querySelectorAll('button');
+const btn = document.querySelectorAll('button');
 
- btn.forEach((button) => {
+btn.forEach((button) => {
     button.addEventListener('click', () => {
-        game(button.id);
+        if (playerScore.textContent === '5' || computerScore.textContent === '5') {
+            button.removeEventListener('click');
+        }
+        else {
+            game(button.id);
+        }
     })
-    
- });
+});
